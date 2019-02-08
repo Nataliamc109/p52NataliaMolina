5c6f30d57da87fac0b5266cd6c64770e5a589b1c
NataliaMolina <molinanatalia483@gmail.com>
2019-02-08 13:24:25 +0100

    Creamos Cliente

---
+++ src/java/daw/ejemploweb/Cliente.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.ejemploweb;
+
+/**
+ *
+ * @author nataliamc
+ */
+public class Cliente {
+    private String nombre;
+    private String email;
+    private int edad;
+}
